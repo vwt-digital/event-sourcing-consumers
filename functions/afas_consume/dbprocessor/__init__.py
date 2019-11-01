f52bdf852d0dbe43e65f04e11893d858cda42807
Bernie van Veen <b.vanveen@vwt.digital>
2019-11-01 15:41:08 +0100

    Allow update on filter_property. DAT-1067

--- functions/afas_consume/dbprocessor/__init__.py
+++ functions/afas_consume/dbprocessor/__init__.py
@@ -1,6 +1,7 @@
 from google.cloud import datastore
 import config
 import os
+import logging
 
 
 class DBProcessor(object):
@@ -10,15 +11,25 @@ class DBProcessor(object):
         pass
 
     def process(self, payload):
-        kind, key = self.identity(payload)
-        entity_key = self.client.key(kind, key)
-        entity = self.client.get(entity_key)
+        if 'id_property' in self.meta and self.meta['id_property'] in payload:
+            kind, key = self.identity(payload)
+            entity_key = self.client.key(kind, key)
+            entity = self.client.get(entity_key)
+            if not entity:
+                entity = datastore.Entity(key=entity_key)
+        elif 'filter_property' in self.meta and self.meta['filter_property'] in payload:
+            # get entity_key from filter property
+            query = self.client.query(kind=self.meta['entity_name'])
+            query.add_filter(self.meta['filter_property'], '=', payload[self.meta['filter_property']])
+            query_results = list(query.fetch(limit=1))
+            entity = query_results[0] if query_results else None
+        else:
+            logging.error('Received payload without matching id_property or filter_property')
+            entity = None
 
-        if entity is None:
-            entity = datastore.Entity(key=entity_key)
-
-        self.populate_from_payload(entity, payload)
-        self.client.put(entity)
+        if entity is not None:
+            self.populate_from_payload(entity, payload)
+            self.client.put(entity)
 
     def identity(self, payload):
         return self.meta['entity_name'], payload[self.meta['id_property']]
