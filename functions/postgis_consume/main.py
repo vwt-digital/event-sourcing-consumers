b2152072cafb0c024701a6e5f89fb1e3979e7877
SpringPie <bente.vandepol@gmail.com>
2020-01-16 16:50:02 +0100

    Python files gave errors because parser does not yet work DAT-1337

--- functions/postgis_consume/main.py
+++ functions/postgis_consume/main.py
@@ -1,15 +1,16 @@
 import logging
 import json
 import base64
+import json
+from flask import Request
 
-from dbprocessor import DBProcessor
+#from dbprocessor import DBProcessor
 
 import config  # noqa: F401
 
-parser = DBProcessor()
+#parser = DBProcessor()
 
 logging.basicConfig(level=logging.INFO)
-
 
 # First json to postgis, then postgis to database
 def json_to_database(request):
@@ -22,7 +23,7 @@ def json_to_database(request):
         subscription = envelope['subscription'].split('/')[-1]
         logging.info(f'Message received from {subscription} [{payload}]')
 
-        parser.process(json.loads(payload))
+        # parser.process(json.loads(payload))
 
     except Exception as e:
         logging.info('Extract of subscription failed')
@@ -36,3 +37,8 @@ def json_to_database(request):
 
 if __name__ == '__main__':
     logging.info("Hallo")
+    environ = {}
+    biepboep = Request(environ)
+    data = base64.b64encode(json.dumps({"name": "John", "age":30, "city":"New York"}).encode('utf-8'))
+    biepboep.data = json.dumps({"message": {"data": data.decode('utf-8')}, "subscription" : "hoi"}).encode('utf-8')
+    json_to_database(biepboep)
