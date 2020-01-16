a327f3226aae152fb298d6aff65fb69c4de99a6e
SpringPie <bente.vandepol@gmail.com>
2020-01-16 15:55:02 +0100

    Changed name of consumer function DAT-1337

--- functions/postgis_consume/main.py
+++ functions/postgis_consume/main.py
@@ -1,37 +1,18 @@
-import inspect
 import logging
 import json
 import base64
-import os
-
-from google.auth.transport import requests
 
 from dbprocessor import DBProcessor
 
-import config
+import config  # noqa: F401
 
 parser = DBProcessor()
-verification_token = os.environ['PUBSUB_VERIFICATION_TOKEN']
-domain_token = config.DOMAIN_VALIDATION_TOKEN
 
 logging.basicConfig(level=logging.INFO)
 
 
-def topic_to_datastore(request):
-    if request.method == 'GET':
-        return '''
-             <html>
-                 <head>
-                     <meta name="google-site-verification" content="{token}" />
-                 </head>
-                 <body>
-                 </body>
-             </html>
-         '''.format(token=domain_token)
-
-    if request.args.get('token', '') != verification_token:
-        return 'Invalid request', 400
-
+# First json to postgis, then postgis to database
+def json_to_database(request):
     # Extract data from request
     envelope = json.loads(request.data.decode('utf-8'))
     payload = base64.b64decode(envelope['message']['data'])
