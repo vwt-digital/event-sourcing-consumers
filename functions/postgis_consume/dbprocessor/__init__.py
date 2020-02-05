0b4784de16df9bb02db046c2a90c1a22c6f59fe7
SpringPie <bente.vandepol@gmail.com>
2020-02-05 11:08:39 +0100

    Changed if statement DAT-1713

--- functions/postgis_consume/dbprocessor/__init__.py
+++ functions/postgis_consume/dbprocessor/__init__.py
@@ -1,8 +1,8 @@
 import config
 import os
-# import psycopg2
-import pg8000  # noqa: F401
-from sqlalchemy import engine
+import psycopg2
+# import pg8000  # noqa: F401
+# from sqlalchemy import engine
 from sqlalchemy import create_engine
 from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy import MetaData
@@ -20,18 +20,18 @@ class DBProcessor(object):
         self.instance_connection_name = os.environ.get('INSTANCE_CONNECTION_NAME')
         self.host = f"/cloudsql/{self.instance_connection_name}"
         self.db_name = os.environ.get("_DB_NAME")
-        # self.engine = create_engine('postgresql+psycopg2://', creator=self.getconn)
-        self.engine = create_engine(
-            engine.url.URL(
-                drivername='postgres+pg8000',
-                username=self.db_user,
-                password=self.sql_pass,
-                database=self.db_name,
-                query={
-                    'unix_sock': '/cloudsql/{}/.s.PGSQL.5432'.format(self.instance_connection_name)
-                }
-            )
-        )
+        self.engine = create_engine('postgresql+psycopg2://', creator=self.getconn)
+        # self.engine = create_engine(
+        #     engine.url.URL(
+        #         drivername='postgres+pg8000',
+        #         username=self.db_user,
+        #         password=self.sql_pass,
+        #         database=self.db_name,
+        #         query={
+        #             'unix_sock': '/cloudsql/{}/.s.PGSQL.5432'.format(self.instance_connection_name)
+        #         }
+        #     )
+        # )
         self.connection = self.engine.connect()
         pass
 
@@ -41,7 +41,7 @@ class DBProcessor(object):
         # If both x and y coordinate in request put request in DB, otherwise do nothing
         if self.meta['x_coordinate'] in selector_data and self.meta['y_coordinate'] in selector_data:
             # Only if x and y are not 0
-            if selector_data[self.meta['x_coordinate']] != 0 and selector_data[self.meta['y_coordinate']] != 0:
+            if selector_data[self.meta['x_coordinate']] != "0" and selector_data[self.meta['y_coordinate']] != "0":
                 try:
                     meta_data = MetaData(bind=self.engine, reflect=True)
                     workflow_projects = meta_data.tables[self.meta['entity_name']]
@@ -69,6 +69,8 @@ class DBProcessor(object):
                 finally:
                     # closing database connection.
                     self.connection.close()
+            else:
+                print("x coordinate or y coordinate is 0, no upload")
 
     def coordinatesToPostgis(self, x_coordinate, y_coordinate):
         # Only points are added now, but what if we want other geometry? Then we should get more info from the JSON
@@ -88,6 +90,6 @@ class DBProcessor(object):
         # Returns the same as when you do: "ST_Transform(ST_SetSRID(ST_MakePoint({x_coordinate},{y_coordinate}),3857),4326)"
         return point
 
-    # def getconn(self):
-    #     c = psycopg2.connect(user=self.db_user, password=self.sql_pass, host=self.host, dbname=self.db_name)
-    #     return c
+    def getconn(self):
+        c = psycopg2.connect(user=self.db_user, password=self.sql_pass, host=self.host, dbname=self.db_name)
+        return c
