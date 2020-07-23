ec344797e7b71d3142decd17662a7380a6dab208
Andre van Turnhout <andre.vanturnhout@viqtordavis.com>
2020-07-23 11:21:24 +0200

    added update date to dashboard DAN-130

--- functions/fttx_consume/deploy.sh
+++ functions/fttx_consume/deploy.sh
@@ -1,7 +1,7 @@
-gcloud functions deploy vwt-d-gew1-fttx-dashboard-consume-func \
+gcloud functions deploy ${PROJECT_ID}-consume-func \
   --entry-point=handler \
   --runtime=python37 \
   --trigger-http \
-  --project=vwt-d-gew1-fttx-dashboard \
+  --project=${PROJECT_ID} \
   --region=europe-west1 \
   --timeout=540s
