0ed6270a89294155b3e75748ee8ff2197c623ccc
Mark Bruisten <markbruisten@Marks-MacBook-Pro.local>
2020-09-04 14:31:57 +0200

    DAT-3728: modified consume function to apply gobits

--- functions/hxs_consume/main.py
+++ functions/hxs_consume/main.py
@@ -348,8 +348,12 @@ def topic_to_cloudsql(request):
 
     try:
         # Extract subscription from subscription string
+        subject = config.TOPIC_SETTINGS.get('subject') if 'subject' in config.TOPIC_SETTINGS else None
         envelope = json.loads(request.data.decode('utf-8'))
-        payload = base64.b64decode(envelope['message']['data'])
+        if subject:
+            payload = base64.b64decode(envelope['message']['data'].get(subject))
+        else:
+            payload = base64.b64decode(envelope['message']['data'])
         subscription = envelope['subscription'].split('/')[-1]
     except Exception as e:
         logging.error('Extract of subscription failed')
